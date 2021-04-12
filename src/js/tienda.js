9c54453dd70823722f87b3614e86d7b3c8643ff4
Mauricio Basurto <mauro@mauro.localdomain>
2021-04-12 15:58:06 -0500

    Agregue contenedor a los articulos de la tienda, de momento esta bien, falta agregarle el source y crear archivo para articulo

--- src/js/tienda.js
+++ src/js/tienda.js
@@ -6,8 +6,28 @@ function cargarTienda() {
         imagen.dataset.imagenId = i;
         // imagen.onclick = mostrarImagen;
 
+        //Contenedor del texto
+        const contenedor = document.createElement('DIV');
+        const titulo = document.createElement('H4');
+        const descripcion = document.createElement('P');
+        const boton = document.createElement('A');
+
+        titulo.textContent = `Test product #${i}`;
+        descripcion.textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla in libero faucibus, pharetra erat id, convallis risus. "
+        boton.textContent = 'Ver articulo';
+
+        contenedor.classList.add('contenedor-articulo');
+        titulo.classList.add('titulo-articulo');
+        descripcion.classList.add('descripcion-articulo');
+        boton.classList.add('boton-tienda');
+
+        contenedor.appendChild(titulo);
+        contenedor.appendChild(descripcion);
+        contenedor.appendChild(boton);
+
         const lista = document.createElement('LI');
         lista.appendChild(imagen);
+        lista.appendChild(contenedor);
         galeria.appendChild(lista);
     }
 
